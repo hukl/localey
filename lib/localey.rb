3cee4851d2dc18296b4fee359010c34208a4e8d9
hukl <contact@smyck.org>
2010-01-03 20:37:28 +0100

    don't know exactly why its gotta be like that but for some reason it didn't work with instance variables in tests

--- lib/localey.rb
+++ lib/localey.rb
@@ -1,9 +1,17 @@
+require 'ruby-debug'
+
 class Localey
   
   def initialize app
     @app      = app
-    @locales  = I18n.available_locales
-    @pattern  = /^\/(#{@locales.map{|l| Regexp.escape(l.to_s)}.join('|')})(?=\/|$)/
+  end
+  
+  def locales
+    @@locales ||= I18n.available_locales
+  end
+  
+  def pattern
+    @@pattern ||= /^\/(#{locales.map{|l| Regexp.escape(l.to_s)}.join('|')})(?=\/|$)/
   end
   
   def call env
@@ -14,11 +22,11 @@ class Localey
   end
   
   def filter_locale env
-    locale = env['PATH_INFO'].match(@pattern)[1] rescue nil
-    
+    locale = env['PATH_INFO'].match(pattern)[1] rescue nil
+
     if locale
       I18n.locale = locale
-      env['PATH_INFO'].sub!(/^\/([\w-]+)\//, "/")
+      env['PATH_INFO'].sub!(pattern, '')
     end
     
     env
